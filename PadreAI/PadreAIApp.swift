665f62cd5c793464187abbfb54082c6c6c81311a
Your Name <odphineguy@gmail.com>
2026-03-11 20:10:32 -0700

    Initial Commit

---
+++ PadreAI/PadreAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadreAIApp.swift
+//  PadreAI
+//
+//  Created by Abe Perez on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PadreAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
